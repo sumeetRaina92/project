02ed876b7eaf881dd9016c7d6dd78d120bce77c4
sumeetRaina92 <119331951+sumeetRaina92@users.noreply.github.com>
2022-12-07 14:39:20 +0530

    Update registrationForm.js

--- registrationForm.js
+++ registrationForm.js
@@ -2,13 +2,12 @@
 
 import React, {useState} from 'react';
 import axios from 'axios';
-
+import DatePicker from 'react-date-picker';
 import './style.css'
 export default RegistrationForm;
 
 function RegistrationForm() {
-    
-    
+   
 const [SFName,setSFName]=useState('');
 const [SLName,setSLName]=useState('');
 const [Sdob,setSdob]=useState('');
@@ -32,7 +31,7 @@ else if(SLName.length==0)
     alert("Student Last Name has Left Blank")
 }
 
-else if(Sdob.length==0)
+else if(valuee.length==0)
 {
     alert("Student Dob has Left Blank")
 }
@@ -95,93 +94,110 @@ else if(address.length==0)
 
 
 }
+
+
+const refreshPage = ()=>{
+    window.location.reload();
+ }
+
+
+
+ const closeTab = () => {
+    window.opener = null;
+    window.open("", "_self");
+    window.close();
+  };
+  
+  const [valuee, onChangee] = useState("");
+ 
+
     return(
             
             <form>
     
              <div  className="2ND">
-                <h2 style={{ color: '#fffafa' }}>New Student Registration Form</h2>
+                <h2 style={{ color: '#FADA06', fontSize: 20,fontFamily: 'Perpetua' }}>New Student Registration Form</h2>
             </div>
       <div className="form">
           <div className="form-body">
           
-                
-              <div className="first">
+            
               <label className="form__label" for="firstName"> STUDENT FIRST NAME </label>
                   <input className="form__input"  autocomplete="off" type="text" id="SFName"  required value={SFName} onChange={(e) => setSFName(e.target.value)}/>
                   
-    </div> 
+    
     
        
     
-    <div className="last">
+    
                   <label  className="form__label" for="LastName"> STUDENT LAST NAME </label>
                   <input className="form__input"  autocomplete="off" type="text" id="SLName"  required value={SLName} onChange={(e) => setSLName(e.target.value)}/>
     
-    </div>
-    
-    
-    <div className="dob">
+        
                   <label  className="form__label" for="LastName"> STUDENT DOB<br></br>[DD-MMM-YYYY] </label>
-                  <input className="form__input"  autocomplete="off" type="date" id="Sdob"  required value={Sdob} onChange={(e) => setSdob(e.target.value)}/>
+                  {/*<input className="form__input"  autocomplete="off" type="date" id="Sdob"  required value={Sdob} onChange={(e) => setSdob(e.target.value)}/>*/}
     
-    </div>
-          
-       
-    
+                  <DatePicker onChange={onChangee} autocomplete="off" id="Sdob" required  value={valuee} format="dd-MMM-yyyy"   />
+                  
+        
+      
   
               
-              <div className="FName">
+             
               <label className="form__label" for="lastName">FATHER NAME </label>
                   <input  type="text"  autocomplete="off" name="" id="Fname"  className="form__input"  required value={Fname} onChange={(e) => setFname(e.target.value)}/>
+           
               
-            </div> 
               
-              <div className="MName">
               <label className="form__label" for="lastName">MOTHER NAME </label>
                   <input className="form__input" type="text"  autocomplete="off" name="" id="Mname"   required  value={Mname} onChange={(e) => setMname(e.target.value)}/>
               
-            </div> 
+          
               
               
                
            
-              <div className="address">
+              
                   <label className="form__label" for="address">ADDRESS</label>
                   <input className="form__input" type="text"  autocomplete="off" id="address"  required value={address} onChange={(e) => setaddress(e.target.value)}/>
-              </div>
-              <div className="village">
+              
+              
                   <label className="form__label" for="VILLAGE">VILLAGE/CITY</label>
                   <input className="form__input" type="text"  autocomplete="off" id="village"  required value={village} onChange={(e) => setvillage(e.target.value)}/>
-              </div>
-              <div className="Ano">
+           
+             
                   <label className="form__label" for="address">AADHAR NO </label>
                   <input className="form__input" type="number"  autocomplete="off" id="pc" required  value={pc} onChange={(e) => setpc(e.target.value)}/>
-              </div>
+         
               
-              <div className="contact">
+              
                   <label className="form__label" for="amount">MOBILE </label>
                   <input className="form__input" type="number"  autocomplete="off" id="contact"  required value={contact} onChange={(e) => setcontact(e.target.value)}/>
-              </div>
+          
               
-              <div className="pin">
+              
                   <label className="form__label" for="amount">PINCODE </label>
                   <input className="form__input" type="number"  autocomplete="off" id="Pin"   required  value={pin} onChange={(e) => setpin(e.target.value)}/>
-              </div>
+              
               
           </div>
        
       </div> 
        <div class="footer">
-              <button onClick={handlesubmit}>submit</button> 
-
+              <button className="form__button" onClick={closeTab}>Clear All and Add New</button>
+              <button className="form__button" onClick={handlesubmit}>Save and Add New</button> 
+              <button className="form__button" onClick={refreshPage}>Reset</button>
               
  
           </div>
           
       </form>
       
-    )       
+    )  
+    
+    
+
+    
 }
         
 
